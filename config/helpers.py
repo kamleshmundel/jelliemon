c17975c0f3ff0a72257005d3aec0916355e7a0e9
kamleshmundel <kamleshmundel18@gmail.com>
2025-10-30 23:27:15 +0530

    gac otp 6 digit

--- config/helpers.py
+++ config/helpers.py
@@ -3,7 +3,7 @@ from django.conf import settings
 import re
 
 def generate_otp():
-    return "1234" if getattr(settings, 'BREACH_OTP', False) else str(random.randint(1000, 9999))
+    return "123456" if getattr(settings, 'BREACH_OTP', False) else str(random.randint(100000, 999999))
 
 def calculate_read_time_in_hours(content, wpm=200):
     words = len(re.findall(r'\w+', content))
