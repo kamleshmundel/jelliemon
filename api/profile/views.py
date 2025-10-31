a74149118dbb3c5d8b2ed5b1cb52c7637c2221ba
DesignDreamAshu <ashutosh@designdream.in>
2025-10-31 23:46:56 +0530

    xp logic fixed

--- api/profile/views.py
+++ api/profile/views.py
@@ -66,7 +66,7 @@ def update_profile(request):
     if 'school' in data: info.school = data['school']
     if 'board' in data: info.board = data['board']
     if 'current_class' in data: info.user_class = data['current_class']
-    if 'xp' in data: info.xp = data['xp']
+    if 'xp' in data: info.xp += data['xp']
 
     if 'country' in data:
         try: info.country = Country.objects.get(id=data['country'])
